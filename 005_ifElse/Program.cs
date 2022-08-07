fa26569c1a8a71fc69bdb0f00408b1407bd58ff5
Рулан <temnihan@mail.ru>
2022-08-07 22:07:01 +0300

    005 if else

---
+++ 005_ifElse/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine(" enter name");
+string username = Console.ReadLine();
+if (username.ToLower() == "ruslan")
+{
+    Console.WriteLine("hello boss");
+
+}
+else
+{
+    Console.Write("hello");
+    Console.Write(username);
+}
